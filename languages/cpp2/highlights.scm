d58b494a3aeb1b7964be960c6e29bf71e0a26ab3
Tsoj Tsoj <tsoj.tsoj@gmx.de>
2024-09-04 01:53:59 +0200

    Added old C++ highlights

--- languages/cpp2/highlights.scm
+++ languages/cpp2/highlights.scm
@@ -1,3 +1,200 @@
+;-----------  C++  -----------;
+
+(field_identifier) @property
+
+(call_expression
+  function: (qualified_identifier
+    name: (identifier) @function))
+
+(call_expression
+  function: (identifier) @function)
+
+(call_expression
+  function: (field_expression
+    field: (field_identifier) @function))
+
+(preproc_function_def
+  name: (identifier) @function.special)
+
+(template_function
+  name: (identifier) @function)
+
+(template_method
+  name: (field_identifier) @function)
+
+(function_declarator
+  declarator: (identifier) @function)
+
+(function_declarator
+  declarator: (qualified_identifier
+    name: (identifier) @function))
+
+(function_declarator
+  declarator: (field_identifier) @function)
+
+(operator_name
+  (identifier)? @operator) @function
+
+(destructor_name (identifier) @function)
+
+((namespace_identifier) @type
+ (#match? @type "^[A-Z]"))
+
+(auto) @type
+(type_identifier) @type
+
+(statement_identifier) @label
+(this) @variable.special
+("static_assert") @function.builtin
+
+[
+  "alignas"
+  "alignof"
+  "break"
+  "case"
+  "catch"
+  "class"
+  "co_await"
+  "co_return"
+  "co_yield"
+  "concept"
+  "constexpr"
+  "continue"
+  "decltype"
+  "default"
+  "delete"
+  "do"
+  "else"
+  "enum"
+  "explicit"
+  "extern"
+  "final"
+  "for"
+  "friend"
+  "if"
+  "inline"
+  "namespace"
+  "new"
+  "noexcept"
+  "override"
+  "private"
+  "protected"
+  "public"
+  "requires"
+  "return"
+  "sizeof"
+  "struct"
+  "switch"
+  "template"
+  "throw"
+  "try"
+  "typedef"
+  "typename"
+  "union"
+  "using"
+  "while"
+  (primitive_type)
+  (sized_type_specifier)
+  (storage_class_specifier)
+  (type_qualifier)
+  (virtual)
+] @keyword
+
+[
+  "#define"
+  "#elif"
+  "#else"
+  "#endif"
+  "#if"
+  "#ifdef"
+  "#ifndef"
+  "#include"
+  (preproc_directive)
+] @keyword
+
+(comment) @comment
+
+[
+  (true)
+  (false)
+  (null)
+  ("nullptr")
+] @constant
+
+(number_literal) @number
+
+[
+  (string_literal)
+  (system_lib_string)
+  (char_literal)
+  (raw_string_literal)
+] @string
+
+[
+  ","
+  ":"
+  "::"
+  ";"
+  (raw_string_delimiter)
+] @punctuation.delimiter
+
+[
+  "{"
+  "}"
+  "("
+  ")"
+  "["
+  "]"
+] @punctuation.bracket
+
+[
+  "."
+  ".*"
+  "->*"
+  "~"
+  "-"
+  "--"
+  "-="
+  "->"
+  "="
+  "!"
+  "!="
+  "|"
+  "|="
+  "||"
+  "^"
+  "^="
+  "&"
+  "&="
+  "&&"
+  "+"
+  "++"
+  "+="
+  "*"
+  "*="
+  "/"
+  "/="
+  "%"
+  "%="
+  "<<"
+  "<<="
+  ">>"
+  ">>="
+  "<"
+  "=="
+  ">"
+  "<="
+  ">="
+  "<=>"
+  "||"
+  "?"
+] @operator
+
+(conditional_expression ":" @operator)
+(user_defined_literal (literal_suffix) @operator)
+
+
+;-----------  Cpp2 -----------;
 
 "++" @operator
 "--" @operator
@@ -30,7 +227,6 @@
 "..<" @operator
 "..=" @operator
 
-; assignment operator @operator
 "=" @operator
 "*=" @operator
 "/=" @operator
@@ -79,13 +275,13 @@
 (macro_comment) @property
 
 (cpp2_block_declaration
-    name: (cpp2_non_template_identifier) @emphasis.strong );strong
+    name: (cpp2_non_template_identifier) @emphasis.strong )
 
 (cpp2_expression_declaration
-    name: (cpp2_non_template_identifier) @emphasis.strong );strong
+    name: (cpp2_non_template_identifier) @emphasis.strong )
 
 (cpp2_no_definition_declaration
-    name: (cpp2_non_template_identifier)  @emphasis.strong);strong
+    name: (cpp2_non_template_identifier)  @emphasis.strong)
 
 (cpp2_function_declaration_argument
   (cpp2_any_identifier
