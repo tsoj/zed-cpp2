4d02d908c1e307caec430ef28da46f15a1a52a7b
Tsoj Tsoj <tsoj.tsoj@gmx.de>
2024-09-04 01:36:03 +0200

    Improved syntax highlighting

--- languages/cpp2/highlights.scm
+++ languages/cpp2/highlights.scm
@@ -17,7 +17,6 @@
 "<=>" @operator
 "<" @operator
 ">" @operator
-"> template" @operator
 "<=" @operator
 ">=" @operator
 "==" @operator
@@ -64,32 +63,72 @@
 
 "->" @function
 
+"template <" @punctuation.bracket
+"> template" @punctuation.bracket
+"(" @punctuation.bracket
+")" @punctuation.bracket
+"[" @punctuation.bracket
+"]" @punctuation.bracket
+"{" @punctuation.bracket
+"}" @punctuation.bracket
+
 (cpp2_primitive_type) @type
 
 (cpp2_number_literal) @number
 
 (macro_comment) @property
 
-(cpp2_no_definition_declaration
-    name: (cpp2_non_template_identifier) @constant.builtin )
-
 (cpp2_block_declaration
-    name: (cpp2_non_template_identifier) @constant.builtin )
+    name: (cpp2_non_template_identifier) @emphasis.strong );strong
 
 (cpp2_expression_declaration
-    name: (cpp2_non_template_identifier) @constant.builtin )
+    name: (cpp2_non_template_identifier) @emphasis.strong );strong
+
+(cpp2_no_definition_declaration
+    name: (cpp2_non_template_identifier)  @emphasis.strong);strong
+
+(cpp2_function_declaration_argument
+  (cpp2_any_identifier
+    last: (cpp2_no_namespace_identifier
+        (cpp2_non_template_identifier) @emphasis)))
+
+(cpp2_function_declaration_argument
+    (cpp2_expression_declaration
+        name: (cpp2_non_template_identifier) @emphasis))
+
+(cpp2_function_declaration_argument
+    (cpp2_block_declaration
+        name: (cpp2_non_template_identifier) @emphasis))
+
+(cpp2_function_declaration_argument
+    (cpp2_no_definition_declaration
+        name: (cpp2_non_template_identifier) @emphasis))
 
 (cpp2_no_definition_declaration
     type: (cpp2_expression
-        (cpp2_any_identifier) @type))
+        (cpp2_any_identifier
+            last: (cpp2_no_namespace_identifier
+                (cpp2_template_identifier
+                    (cpp2_non_template_identifier) @type)))))
+
+(cpp2_no_definition_declaration
+    type: (cpp2_expression
+        (cpp2_any_identifier
+            last: (cpp2_no_namespace_identifier) @type)))
 
 (cpp2_left_side_of_definition
     type: (cpp2_expression
-        (cpp2_any_identifier) @type))
+        (cpp2_any_identifier
+            last: (cpp2_no_namespace_identifier
+                (cpp2_template_identifier
+                    (cpp2_non_template_identifier) @type)))))
 
 (cpp2_function_type
     return: (cpp2_expression
-        (cpp2_any_identifier) @type))
+        (cpp2_any_identifier
+            last: (cpp2_no_namespace_identifier
+                (cpp2_template_identifier
+                    (cpp2_non_template_identifier) @type)))))
 
 (cpp2_next) @keyword
 
